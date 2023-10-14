6eff670de2242eee198b7a30498c1eeb8510edb7
Rishi Raj <rishiraj11a@gmail.com>
2023-10-15 01:35:52 +0530

    updating StudyPT and Youtube Section

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:focusio/views/screens/users/Signin.dart';
 import 'package:focusio/views/widgets/Colors.dart';
 import 'package:focusio/views/widgets/bottomBar.dart';
 import 'firebase_options.dart';
+
+Future<void> StudyPtScreen() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,25 +18,31 @@ void main() async {
   return runApp(const MyApp());
 }
 
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await dotenv.load(fileName: ".env");
+//   runApp(const MyApp());
+// }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: const ColorScheme.dark(
-            background: darkLevel1, brightness: Brightness.light),
-      ),
-      debugShowCheckedModeBanner: false,
-      home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot) {
-        if(snapshot.hasData) {
-          return const BottomBarWidget();
-        }
-        else {
-          return const SignInScreen();
-        }
-      })
+        theme: ThemeData(
+          colorScheme: const ColorScheme.dark(
+              background: darkLevel1, brightness: Brightness.light),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot) {
+          if(snapshot.hasData) {
+            return const BottomBarWidget();
+          }
+          else {
+            return const SignInScreen();
+          }
+        })
     );
   }
 }
--- lib/views/screens/bottombar/youtube_screen.dart
+++ lib/views/screens/bottombar/youtube_screen.dart
@@ -1,12 +1,177 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:focusio/views/models/channel_model.dart';
+import 'package:focusio/views/models/video_model.dart';
+import 'package:focusio/views/screens/video_screen.dart';
+import 'package:focusio/views/widgets/api_services.dart';
 
-class YoutubeScreen extends StatelessWidget {
+class YoutubeScreen extends StatefulWidget {
   const YoutubeScreen({super.key});
 
   @override
+  _YoutubeScreenState createState() => _YoutubeScreenState();
+}
+
+class _YoutubeScreenState extends State<YoutubeScreen> {
+  late Channel _channel;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initChannel();
+  }
+
+  _initChannel() async {
+    Channel channel = await APIService.instance
+        .fetchChannel(channelId: 'UCeVMnSShP_Iviwkknt83cww');
+    setState(() {
+      _channel = channel;
+    });
+  }
+
+  _buildProfileInfo() {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
+      height: 100.0,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(0, 1),
+            blurRadius: 6.0,
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 35.0,
+            backgroundImage: NetworkImage(_channel.profilePictureUrl),
+          ),
+          const SizedBox(width: 12.0),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _channel.title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  '${_channel.subscriberCount} subscribers',
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _buildVideo(Video video) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => VideoScreen(id: video.id),
+        ),
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+        padding: const EdgeInsets.all(10.0),
+        height: 140.0,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 1),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Row(
+          children: <Widget>[
+            Image(
+              width: 150.0,
+              image: NetworkImage(video.thumbnailUrl),
+            ),
+            const SizedBox(width: 10.0),
+            Expanded(
+              child: Text(
+                video.title,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _loadMoreVideos() async {
+    _isLoading = true;
+    List<Video> moreVideos = await APIService.instance
+        .fetchVideosFromPlaylist(playlistId: _channel.uploadPlaylistId);
+    List<Video> allVideos = _channel.videos..addAll(moreVideos);
+    setState(() {
+      _channel.videos = allVideos;
+    });
+    _isLoading = false;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _channel != null
+          ? NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollDetails) {
+          if (!_isLoading &&
+              _channel.videos.length != int.parse(_channel.videoCount) &&
+              scrollDetails.metrics.pixels ==
+                  scrollDetails.metrics.maxScrollExtent) {
+            _loadMoreVideos();
+          }
+          return false;
+        },
+        child: ListView.builder(
+          itemCount: 1 + _channel.videos.length,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return _buildProfileInfo();
+            }
+            Video video = _channel.videos[index - 1];
+            return _buildVideo(video);
+          },
+        ),
+      )
+          : Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(
+            Theme.of(context).primaryColor, // Red
+          ),
+        ),
+      ),
+    );
   }
 }
